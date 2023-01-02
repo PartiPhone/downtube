dabbdf3348c3cd0fb4b2289ded665193a3256f26
User <user@example.com>
2023-01-02 11:57:07 +0500

    Создание модели

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from downtube.users.models import User
+
+
+class Video(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+    )
+    url = models.CharField(max_length=120, verbose_name="Youtube ссылка")
+    title = models.TextField(verbose_name="Название")
+    video = models.TextField(verbose_name="Видео")
+
+    def save(self, *args, **kwargs):
+        user_streams = Video.objects.filter(user=self.user)
+        if not user_streams.exists():
+            super().save(*args, **kwargs)
+        if not user_streams.filter(url=self.url).exists():
+            if user_streams.count() >= 2:
+                delete_stream = user_streams.first()
+                delete_stream.delete()
+            super().save(*args, **kwargs)
